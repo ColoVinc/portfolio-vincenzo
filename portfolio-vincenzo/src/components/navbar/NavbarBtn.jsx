1a28245cfbd02d99f1049506307a5c0203fd0dff
ColoVinc <149695389+ColoVinc@users.noreply.github.com>
2025-05-20 20:38:28 +0000

    -Creazione della navbar

---
+++ portfolio-vincenzo/src/components/navbar/NavbarBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GoArrowDownRight } from "react-icons/go";
+
+const NavbarBtn = () => {
+    return (
+        <button className='px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow'>
+            Hire Me
+            <GoArrowDownRight />
+        </button>
+    )
+}
+
+export default NavbarBtn
