7cf1b1460a49e0cf748e36d850607d7286fceee6
ColoVinc <149695389+ColoVinc@users.noreply.github.com>
2025-05-22 19:39:09 +0000

    -Aggiunta del componente hero

--- portfolio-vincenzo/src/App.jsx
+++ portfolio-vincenzo/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-import NavbarMain from './components/navbar/NavbarMain'
+import HeroGradient from './components/heroSection/HeroGradient';
+import HeroMain from './components/heroSection/HeroMain';
+import NavbarMain from './components/navbar/NavbarMain'; 
+import SubHeroSection from './components/heroSection/SubHeroSection';
+import HelperSection from './components/HelperSection';
 
 function App() {
   return ( 
     <main className='font-body'>
       <NavbarMain />
+      <HeroMain />
+      <HeroGradient />
+      <SubHeroSection />
+      <HelperSection />
     </main>
   );
 }
