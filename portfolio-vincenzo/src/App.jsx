d68dee567da4c2be9f55a957cddda0e14e3aa6d2
ColoVinc <149695389+ColoVinc@users.noreply.github.com>
2025-05-25 14:55:01 +0000

    -Aggiunta del componente footer

--- portfolio-vincenzo/src/App.jsx
+++ portfolio-vincenzo/src/App.jsx
@@ -10,6 +10,7 @@ import SubSkills from './components/skillsSection/SubSkills';
 import ExperienceMain from './components/experienceSection/ExperienceMain';
 import ProjectsMain from './components/projectsSection/ProjectsMain';
 import ContactMeMain from './components/contactMeSection/ContactMeMain';
+import FooterMain from './components/footer/FooterMain';
 
 function App() {
   return ( 
@@ -24,6 +25,7 @@ function App() {
       <ExperienceMain />
       <ProjectsMain />
       <ContactMeMain />
+      <FooterMain />
       <HelperSection />
     </main>
   );
